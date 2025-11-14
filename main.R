5de0a285eb28fef5eae9aecee947349317804efc
Collins Audi <42319820+audicollins-96@users.noreply.github.com>
2025-11-14 21:37:20 +0000

    filtered by age

--- main.R
+++ main.R
@@ -3,8 +3,10 @@ library(tidyverse)
 library(here)
 
 tv_hours_table <- gss_cat %>% 
-  filter(age >= 30) %>% 
+  filter (age >= 30) %>% 
   group_by (marital) %>% 
   summarize(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
 write_csv(tv_hours_table, here ("TV_hours_by_marital.csv"))
+ 
+
