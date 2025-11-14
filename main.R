0619e8c113af39b9239c183a0f06dd8bc5e4b7bb
Collins Audi <42319820+audicollins-96@users.noreply.github.com>
2025-11-14 21:39:51 +0000

    revised again

--- main.R
+++ main.R
@@ -7,6 +7,6 @@ tv_hours_table <- gss_cat %>%
   group_by (marital) %>% 
   summarize(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
-write_csv(tv_hours_table, here ("TV_hours_by_marital.csv"))
+write_csv(tv_hours_table, here ("TVhours_by_marital.csv"))
  
 
