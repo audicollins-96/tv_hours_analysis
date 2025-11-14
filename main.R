9954d6462d1d5d3ab5cfb19b8f897e5085b51715
Collins Audi <42319820+audicollins-96@users.noreply.github.com>
2025-11-14 22:40:37 +0000

    shorten names

--- main.R
+++ main.R
@@ -2,11 +2,11 @@
 library(tidyverse)
 library(here)
 
-tv_hours_table <- gss_cat %>% 
+tv_hours_tab <- gss_cat %>% 
   filter (age < 30) %>% 
   group_by (marital) %>% 
   summarize(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
-write_csv(tv_hours_table, here ("TVhours_by_marital.csv"))
+write_csv(tv_hours_tab, here ("TVhours_by_marital.csv"))
  
 
